bfb4fa63c05cf83a990ee26797ff2a44da75c2f8
JuanCamilo <juanmddb@gmail.com>
2025-02-19 11:55:44 -0500

    Actualización de settings.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 
-cred = credentials.Certificate("ruta/a/tu/clave.json")
+cred = credentials.Certificate("D:\JuanM\Downloads\pagina\miweb\config\fb.json")
 firebase_admin.initialize_app(cred)
