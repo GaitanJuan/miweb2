54b908103943552ffb1d207866c160d0bf8942e1
JuanCamilo <juanmddb@gmail.com>
2025-02-18 20:12:23 -0500

    Actualización de settings.py

--- miweb/settings.py
+++ miweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a*^zb1i155@8i(r*qlh^)r_m)^@dnsa+4cjic^al2z!+d756ex
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["miweb-hqp6.onrender.com"]
 
 
 # Application definition
