9b3b923be22d1b4fd55ffe207546d67cd9a7e417
JuanCamilo <juanmddb@gmail.com>
2025-02-19 12:02:53 -0500

    Actualización de settings.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,5 +1,16 @@
+import os
+import json
 import firebase_admin
-from firebase_admin import credentials, auth
+from firebase_admin import credentials
 
-cred = credentials.Certificate("D:\JuanM\Downloads\pagina\miweb\config\fb.json")
-firebase_admin.initialize_app(cred)
+# Cargar la ruta desde una variable de entorno
+firebase_cred_path = os.getenv("FIREBASE_CREDENTIALS")
+
+# Cargar las credenciales desde la variable
+if firebase_cred_path and os.path.exists(firebase_cred_path):
+    with open(firebase_cred_path) as f:
+        cred_data = json.load(f)
+    cred = credentials.Certificate(cred_data)
+    firebase_admin.initialize_app(cred)
+else:
+    raise Exception("Las credenciales de Firebase no se encuentran.")
